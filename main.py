b8be6e82eae6a119350b87c341361942b71c007d
KarolG <gitesiek@gmail.com>
2024-02-19 18:41:15 +0100

    finished the game, ai model and main menu

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import sys
 import random
+import neat
 
 WIDTH = 600
 HEIGHT = 400
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+MAX_FITNESS = 1000000000
+best_agent_id = "49"
 
 
 class Bird:
@@ -99,14 +102,20 @@ class Score:
         self.points = 0
         self.font = pygame.font.SysFont(None, 36)
         self.color = (255, 255, 255)
+        self.max_points = 0
 
     def add_point(self):
         self.points = self.points + 1
+        if self.points > self.max_points:
+            self.max_points = self.points
 
     def draw(self):
         score_text = self.font.render(f"Score: {round(self.points,2)}",
                                       True, self.color)
         WINDOW.blit(score_text, (10, 10))
+        max_score_text = self.font.render(f"Max: {round(self.max_points,2)}",
+                                          True, self.color)
+        WINDOW.blit(max_score_text, (150, 10))
 
 
 class Game:
@@ -135,12 +144,83 @@ class Game:
                         self.bird.flap()
                 elif event.type == pygame.USEREVENT:
                     self.new_pipe_pair_event()
-
-            WINDOW.fill((0, 191, 255))
             self.move_and_draw()
             self.reward_or_death()
             pygame.display.update()
+            WINDOW.fill((0, 191, 255))
             pygame.time.Clock().tick(60)
+
+    def plAI(self):
+        self.config_path = "config.txt"
+        self.config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                  neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                  self.config_path)
+        self.population = neat.Population(self.config)
+        self.population.add_reporter(neat.StdOutReporter(True))
+        self.fitness_threshold = 1000000
+        self.generation = 0
+        self.checkpointer = neat.Checkpointer(filename_prefix='flappy_agent_')
+
+        while True:
+            self.population.run(self.eval_genomes, 1)
+            if self.generation % 5 == 0:
+                self.save_checkpoint()
+            self.generation += 1
+
+    def get_inputs(self):
+        bird_y = self.bird.y
+        bird_x = self.bird.x
+        pipe_x = self.pipe_pairs[0].upper_pipe.x
+        pipe_y = self.pipe_pairs[0].upper_pipe.height
+        return [bird_y, bird_x, pipe_x, pipe_y]
+
+    def save_checkpoint(self):
+        self.checkpointer.save_checkpoint(config=self.config, population=self.population.population, species_set=self.population.species, generation=self.generation)
+
+    def load_checkpoint(self):
+        restored_population = self.checkpointer.restore_checkpoint('flappy_agent_'+str(best_agent_id))
+        self.population = restored_population
+        self.population.add_reporter(neat.StdOutReporter(True))
+        self.generation = int(best_agent_id)
+
+    def eval_genomes(self, genomes, config):
+        for genome_id, genome in genomes:
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            self.reset()
+            fitness = 0
+            pygame.time.set_timer(pygame.USEREVENT, 1500)
+            alive_for = 0
+
+            while True:
+                alive_for_add = 0
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_SPACE:
+                            self.bird.flap()
+                        elif event.key == pygame.K_s:
+                            self.save_checkpoint()
+                        elif event.key == pygame.K_l:
+                            self.load_checkpoint()
+                    elif event.type == pygame.USEREVENT:
+                        self.new_pipe_pair_event()
+                inputs = self.get_inputs()
+                output = net.activate(inputs)
+                if output[0] > 0.5:
+                    self.bird.flap()
+                self.move_and_draw()
+                fitness_add, alive_for_add = self.reward_or_death()
+                alive_for += alive_for_add
+                if alive_for > 4000:
+                    break
+                fitness += fitness_add
+
+                pygame.display.update()
+                WINDOW.fill((0, 191, 255))
+                pygame.time.Clock().tick(60)
+            genome.fitness = fitness
 
     def move_and_draw(self):
         self.bird.move()
@@ -153,13 +233,18 @@ class Game:
     def reward_or_death(self):
         if self.bird.collided_with(self.pipe_pairs[0]):
             self.reset()
+            return (-100, 4000)
         elif self.bird.passed_pipes(self.pipe_pairs[0]):
             self.score.add_point()
+            return (self.score.points * 1000, 1)
+        if (self.bird.y < 0 or self.bird.y + self.bird.height > HEIGHT):
+            self.reset()
+            return (-2000, 4001)
         if (self.pipe_pairs[0].upper_pipe.x +
            self.pipe_pairs[0].upper_pipe.width < 0):
             self.pipe_pairs.pop(0)
-        if (self.bird.y < 0 or self.bird.y + self.bird.height > HEIGHT):
-            self.reset()
+            return 0, 0
+        return 1, 0
 
     def reset(self):
         self.pipe_pairs = []
@@ -171,26 +256,18 @@ class Game:
         pygame.time.set_timer(pygame.USEREVENT, 1500)
 
 
-class QAgent():
-    def __init__(self):
-        pass
-
-    def update_q_value(self):
-        pass
-
-    def get_best_action(self):
-        pass
-
-    def save_model(self):
-        pass
-
-    def load_model(self):
-        pass
-
-
 def main():
-    flappyBird = Game()
-    flappyBird.play()
+    flappy_bird_game = Game()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    flappy_bird_game.play()
+                elif event.key == pygame.K_a:
+                    flappy_bird_game.plAI()
 
 
 if __name__ == "__main__":
